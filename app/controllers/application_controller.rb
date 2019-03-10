e183f4329f242ac74c1c2502eb51b0c0c043d65e
kosei <ryu.go.off@gmail.com>
2019-03-10 15:05:16 +0900

    Improve minor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "hello, world!"
   end
 
   def goodbye
     render html: "goodbye, world!"
   end
-  
+
 end
